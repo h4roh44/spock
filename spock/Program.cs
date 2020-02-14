54faa7373da4769bda46031b43265c214165b76f
Henry Rohlfing <henry.rohlfing@okstate.edu>
2020-02-13 20:15:15 -0600

    Evening work on match 1 if statement and logic.

--- spock/Program.cs
+++ spock/Program.cs
@@ -10,34 +10,67 @@ namespace spock
     {
         static void Main(string[] args)
         {
-            // Wlecome and ask the user how many games they would like to play.
+            //
+            // Welcome and ask the user how many games they would like to play.
             //
 
-            Console.WriteLine("Welcome to the game of Rock Paper Scissors Lizard Spock" + "\n" + "\n" + "\n" + "How many matches do you want for this game set ? Please select from 1 or 3 or 5: ");
+            Console.Write("Welcome to the game of Rock Paper Scissors Lizard Spock" + "\n" + "\n" + "\n" + "How many matches do you want for this game set ? Please select from 1 or 3 or 5: ");
 
             string GameNumSelect = Console.ReadLine();
 
-            // Give invalid input if user input is not equal to 1 3 or 5
+            //
+            // Declare Variables for each game option
+            //
+
+            string r = "r";
+            string p = "p";
+            string s = "s";
+            string l = "l";
+            string k = "k";
+
+            //
+            // Convert game number selection to an integer value.
             //
 
             int GameNumSelectConv = Convert.ToInt32(GameNumSelect);
 
-            if (GameNumSelectConv != 1)
-            {
-                Console.WriteLine("Oops, invalid input. Please choose from 1, 3, and 5." + "\n" + "\n" + "\n" + "Press any key to continue.");
-            }
-            else if (GameNumSelectConv != 3)
-            {
-                Console.WriteLine("Oops, invalid input. Please choose from 1, 3, and 5." + "\n" + "\n" + "\n" + "Press any key to continue.");
-            }
+            //
+            // Make variable for game selection
+            //
 
-            else if (GameNumSelectConv < 10)
-            {
-                Console.WriteLine("Oops, invalid input. Please choose from 1, 3, and 5." + "\n" + "\n" + "\n" + "Press any key to continue.");
+            string onematch = "1";
+            string twomatch = "2";
+            string threematch = "3";
+            
+            //
+            //Match 1 if statement
+            //
 
+            if (GameNumSelectConv == 1)
+            {
+            Console.Write("Match 1" + "\n" + "\n" + "Enter (r)ock, (p)aper, (s)cissors, (l)izard, or spoc(k):");
+            string userChoice = Console.ReadLine();
+                
             }
+                else if (GameNumSelect != onematch)
+                {
+                Console.WriteLine("Oops, invalid input. Please choose from 1, 3, and 5." + "\n" + "\n" + "\n" + "Press any key to continue.");
+                }
+                else if (GameNumSelect != twomatch)
+                {
+                Console.WriteLine("Oops, invalid input. Please choose from 1, 3, and 5." + "\n" + "\n" + "\n" + "Press any key to continue.");
+                }
+                else if (GameNumSelect != threematch)
+                {
+                Console.WriteLine("Oops, invalid input. Please choose from 1, 3, and 5." + "\n" + "\n" + "\n" + "Press any key to continue.");
+                }
+
+    
+            //
+            // Give invalid input if user input is not equal to 1 3 or 5
+            //
 
             Console.ReadKey();
+            }
         }
-    }
-}
+    } 
